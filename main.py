fd5b350a066d697217618b3dffb28babf5fbc17d
Szymcio587 <szymon.talar2882@gmail.com>
2023-04-19 11:26:01 +0200

    Code refactoring + adding MLP Classifier

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import warnings
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix,classification_report
+from sklearn.metrics import classification_report
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neural_network import MLPClassifier
 
 warnings.filterwarnings("ignore")
 
@@ -17,55 +15,51 @@ columns = ["cap-shape","cap-surface","cap-color","bruises%3F"
      "stalk-surface-below-ring","stalk-color-above-ring","stalk-color-below-ring","veil-type",
      "veil-color","ring-number","ring-type","spore-print-color","population","habitat","class"]
 
-data = pd.read_csv("mushroom.csv")
+def getData():
+    data = pd.read_csv("mushroom.csv")
+    labelEncoder = LabelEncoder()
 
-labelEncoder = LabelEncoder()
+    for i in columns:
+        data[i] = labelEncoder.fit_transform(data[i])
 
-for i in columns:
-    data[i] = labelEncoder.fit_transform(data[i])
+    X = data.drop(['class'], axis=1)
+    Y = data['class']
 
-correlation = data.corr()
+    print("Data has been loaded...\n")
+    return train_test_split(X, Y, test_size=0.2, random_state=101)
 
-plt.figure(figsize = (20,10),dpi = 200)
-sns.heatmap(correlation,annot = True,cmap = 'viridis')
+def printTestValues(X_train, X_test, Y_train, Y_test):
+    classifier = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+    classifier.fit(X_train, Y_train)
+    prediction = classifier.predict(X_test)
+    print("Train set values:\n")
+    print(classification_report(Y_test, prediction))
 
-plt.figure(figsize=(20,10),dpi=200)
-sns.barplot(data = correlation)
-plt.xticks(rotation=90)
 
-X = data.drop(['class'],axis=1)
-Y = data['class']
+def ADAPredict(X_train, X_test, Y_train, Y_test):
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 101)
+    boostClasifier = AdaBoostClassifier(n_estimators = 10)
+    boostClasifier.fit(X_train, Y_train)
+    BCPrediction = boostClasifier.predict(X_test)
+    boostClasifier.feature_importances_.argmax()
+    print("Test set values predicted with ADA:\n")
+    print(classification_report(BCPrediction, Y_test))
 
-classifier = RandomForestClassifier()
+def MLPPredict(X_train, X_test, Y_train, Y_test):
+    #MLPclassifier = MLPClassifier(hidden_layer_sizes = (50,), max_iter = 100, verbose = 2137, learning_rate_init = 0.1, learning_rate = 'adaptive', solver = 'sgd')
+    MLPclassifier = MLPClassifier(hidden_layer_sizes = (10,), max_iter = 50)
+    MLPclassifier.fit(X_train, Y_train)
+    Y_pred = MLPclassifier.predict(X_test)
+    print("Test set values predicted with MLP:\n")
+    print(classification_report(Y_pred, Y_test))
 
-parameters = {'n_estimators':[100,200,300],
-           'criterion' :['gini', 'entropy'],
-           'max_features':['auto', 'sqrt', 'log2']}
+def makePrediction():
+    X_train, X_test, Y_train, Y_test = getData()
 
-grid=GridSearchCV(classifier,parameters)
+    printTestValues(X_train, X_test, Y_train, Y_test)
 
-grid.fit(X_train,Y_train)
+    ADAPredict(X_train, X_test, Y_train, Y_test)
+    MLPPredict(X_train, X_test, Y_train, Y_test)
 
-classifier.fit(X_train,Y_train)
-
-prediction = classifier.predict(X_test)
-
-print(classification_report(Y_test,prediction))
-
-plt.figure(figsize = (20,10),dpi = 200)
-confusionMatrix = confusion_matrix(Y_test,prediction)
-sns.heatmap(confusionMatrix,annot = True,cmap = 'mako')
-
-boostClasifier = AdaBoostClassifier(n_estimators = 1)
-
-boostClasifier .fit(X_train,Y_train)
-
-BCPrediction = boostClasifier .predict(X_test)
-
-boostClasifier .feature_importances_.argmax()
-
-sns.countplot(data = data,x = 'gill-color',hue = 'class')
-
-print(classification_report(BCPrediction,Y_test))
+if __name__ == "__main__":
+    makePrediction()
