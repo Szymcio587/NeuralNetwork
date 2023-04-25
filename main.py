bb73d613f54016ae8c9e15f8e37f560e19e40c9f
Szymcio587 <szymon.talar2882@gmail.com>
2023-04-25 16:24:24 +0200

    Few prediction types added + generating charts for prediction losses

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import warnings
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -28,6 +29,16 @@ def getData():
     print("Data has been loaded...\n")
     return train_test_split(X, Y, test_size=0.2, random_state=101)
 
+def CreateChart(loss_history, label_name, fin, name="dafault.png"):
+    #plt.plot(range(len(loss_history)), loss_history)
+    sub = plt.subplot()
+    sub.plot(range(len(loss_history)), loss_history, label=label_name)
+    sub.legend()
+    plt.xlabel('Generation')
+    plt.ylabel('Loss')
+    if(fin == True):
+        plt.savefig(name)
+
 def printTestValues(X_train, X_test, Y_train, Y_test):
     classifier = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
     classifier.fit(X_train, Y_train)
@@ -45,13 +56,43 @@ def ADAPredict(X_train, X_test, Y_train, Y_test):
     print("Test set values predicted with ADA:\n")
     print(classification_report(BCPrediction, Y_test))
 
-def MLPPredict(X_train, X_test, Y_train, Y_test):
-    #MLPclassifier = MLPClassifier(hidden_layer_sizes = (50,), max_iter = 100, verbose = 2137, learning_rate_init = 0.1, learning_rate = 'adaptive', solver = 'sgd')
-    MLPclassifier = MLPClassifier(hidden_layer_sizes = (10,), max_iter = 50)
-    MLPclassifier.fit(X_train, Y_train)
+def MLPPredictBasic(X_train, X_test, Y_train, Y_test):
+    MLPclassifier = MLPClassifier(hidden_layer_sizes = (2,), max_iter = 100, verbose = 1)
+    history = MLPclassifier.fit(X_train, Y_train)
+    loss_history = history.loss_curve_
     Y_pred = MLPclassifier.predict(X_test)
-    print("Test set values predicted with MLP:\n")
+    print("Test set values predicted with MLP basic setup(2 neurons, 10 iterations):\n")
     print(classification_report(Y_pred, Y_test))
+    CreateChart(loss_history, "Basic", False)
+
+def MLPPredictPrecise(X_train, X_test, Y_train, Y_test):
+    MLPclassifier = MLPClassifier(hidden_layer_sizes = (20,), max_iter = 100, verbose = 1)
+    history = MLPclassifier.fit(X_train, Y_train)
+    loss_history = history.loss_curve_
+    Y_pred = MLPclassifier.predict(X_test)
+    print("Test set values predicted with MLP precise setup(15 neurons, 50 iterations):\n")
+    print(classification_report(Y_pred, Y_test))
+    CreateChart(loss_history, "Precise", False)
+
+def MLPPredictSGD(X_train, X_test, Y_train, Y_test):
+    MLPclassifier = MLPClassifier(hidden_layer_sizes=(50,), max_iter=100, verbose=2137, learning_rate_init=0.05,
+                                  learning_rate='adaptive', solver='sgd')
+    history = MLPclassifier.fit(X_train, Y_train)
+    loss_history = history.loss_curve_
+    Y_pred = MLPclassifier.predict(X_test)
+    print("Test set values predicted with MLP custom setup(sgd solver, slow learning rate with adaptive abilities):\n")
+    print(classification_report(Y_pred, Y_test))
+    CreateChart(loss_history, "SDG", False)
+
+def MLPPredictSGDOvertrained(X_train, X_test, Y_train, Y_test):
+    MLPclassifier = MLPClassifier(hidden_layer_sizes=(50,), max_iter=100, verbose=2137, learning_rate_init=0.2, solver='sgd')
+    history = MLPclassifier.fit(X_train, Y_train)
+    loss_history = history.loss_curve_
+    Y_pred = MLPclassifier.predict(X_test)
+    print("Test set values predicted with MLP custom setup(sgd solver, too high learning rate for given problem):\n")
+    print(classification_report(Y_pred, Y_test))
+    CreateChart(loss_history, "Overtrained", True, "MLP_plots.png")
+
 
 def makePrediction():
     X_train, X_test, Y_train, Y_test = getData()
@@ -59,7 +100,11 @@ def makePrediction():
     printTestValues(X_train, X_test, Y_train, Y_test)
 
     ADAPredict(X_train, X_test, Y_train, Y_test)
-    MLPPredict(X_train, X_test, Y_train, Y_test)
+
+    MLPPredictBasic(X_train, X_test, Y_train, Y_test)
+    MLPPredictPrecise(X_train, X_test, Y_train, Y_test)
+    MLPPredictSGD(X_train, X_test, Y_train, Y_test)
+    MLPPredictSGDOvertrained(X_train, X_test, Y_train, Y_test)
 
 if __name__ == "__main__":
     makePrediction()
